857b431391c05625c905db173d831ee35239c057
maiqel <michaelolowe321@gmail.com>
2024-10-19 02:37:04 +0100

    refactor: deleted landing page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
-import LandingPage from "./components/LandingPage";
+"use client";
+
+import About from "./components/About";
+import FAQ from "./components/FAQ";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
     <div>
-      <LandingPage />
+      <div className='min-h-screen bg-slate-300'>
+        <Navbar />
+        <Hero />
+        <Stats />
+        <About />
+        <Services />
+        <Testimonials />
+        <FAQ />
+        <Footer />
+        <ScrollToTop />
+      </div>
     </div>
   );
 }
